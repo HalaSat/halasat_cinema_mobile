01fa35c7084afa1f20e8ba98cff78781d41eaf79
Mohammed Salman <msal4@outlook.com>
2019-02-21 17:32:07 +0300

    Fixed search function name

--- lib/src/services/search.dart
+++ lib/src/services/search.dart
@@ -4,11 +4,12 @@ import '../models/post_list.dart';
 import '../const.dart';
 
 
-Future<PostList> fetchFeatured(String query, int page) async {
+Future<PostList> searchPost(String query, int page) async {
   final Map<String, dynamic> data = await fetch(
     voduApiPrefix,
     '/api/list/search/$query/$page',
   );
+
   final PostList postList =
       serializers.deserializeWith(PostList.serializer, data);
 
